865d4a764aad79626c7ef5105f7acbffe0c76fad
Daniel <danielsamet98@gmail.com>
2020-04-13 18:11:44 +0100

    updated the solver runner code;
    Signed-off-by: Daniel <danielsamet98@gmail.com>

--- solver.py
+++ solver.py
@@ -14,20 +14,35 @@ def generate_valid_words(board, dictionary_words):
 
 
 if __name__ == '__main__':
-    board_dice = random.sample(Config.DICE, len(Config.DICE))
-    board = []
+    def generate_board(dice=None):
+        if not dice:
+            board_dice = random.sample(Config.DICE, len(Config.DICE))
 
-    for _ in range(4):
-        board.append([random.choice(board_dice.pop()) for __ in range(4)])
+            board = []
+            for _ in range(4):
+                board.append([random.choice(board_dice.pop()) for __ in range(4)])
 
-    boggle_board = BoggleBoard(board)
+        else:
+            board = [[dice[i + j] for j in range(4)] for i in range(4)]
+
+        return BoggleBoard(board)
+
+
+    def run_generator(board):
+        start = time.time()
+        word_list = generate_valid_words(generate_board().board, words)
+        end = time.time()
+
+        print(f"{len(word_list)} words were generated in {end - start:.6f} seconds")
+
 
     with open("words_alpha.txt") as file:
         words = file.read().split("\n")
 
-    start = time.time()
-    word_list = generate_valid_words(boggle_board.board, words)
+    print("\nPreconfigured board:")
+    print("-" * 20)
+    run_generator(generate_board("LOPGPOCIHBIEGKLS"))
 
-    end = time.time()
-
-    print(f"{len(word_list)} words were generated in {end - start:.6f} seconds")
+    print("\nRandom board:")
+    print("-" * 20)
+    run_generator(generate_board())
